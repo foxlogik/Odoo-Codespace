d45e61582aed75f0d03d3c3ce0152e2fcf5ba229
alaya-aymen <71433088+alaya-aymen@users.noreply.github.com>
2025-06-04 08:16:21 +0000

    handle default values from action context

--- custom-addons/matrix_view/static/src/matrix_model.js
+++ custom-addons/matrix_view/static/src/matrix_model.js
@@ -188,6 +188,13 @@ export class MatrixModel extends Model {
     async _createEmptyRowData() {
         console.log("Creating empty row data");
         const defaults = await this.orm.call(this.metaData.resModel, "default_get", [this.metaData.rowGroupBys.map(gb => gb.split(':')[0])]);
+        console.log("this.searchParams.context",this.searchParams.context)
+        for (const [key, val] of Object.entries(this.searchParams.context)) {
+            if (key.startsWith("default_")) {
+                const fieldName = key.slice(8);  // Remove "default_" prefix
+                defaults[fieldName] = val;
+            }
+        }  
         console.log("defaults",defaults)
         const data = {};
         //this.metaData.rowGroupBys.forEach(groupBy => {
